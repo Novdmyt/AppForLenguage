028703eaa1e6b7bf46d41817d7511a000e0a0c2e
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2024-07-17 21:53:32 +0300

    добавлен значек в сообщения

--- app/src/main/java/com/android/appforlanguage/fragment/FragmentCreate.java
+++ app/src/main/java/com/android/appforlanguage/fragment/FragmentCreate.java
@@ -1,13 +1,19 @@
 package com.android.appforlanguage.fragment;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -49,13 +55,13 @@ public class FragmentCreate extends Fragment {
         buttonPlayGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openFragment(new AddWord());
+                openFragment(new PlayGame());
             }
         });
         buttonNewWord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFragment(new PlayGame());
+                openFragment(new AddWord());
             }
         });
         buttonDictionary.setOnClickListener(new View.OnClickListener() {
@@ -95,22 +101,83 @@ public class FragmentCreate extends Fragment {
                 dataBase.createTable(db, tableName);
                 showCustomToast();  // Викликаємо користувацький Toast
                 tableNameEditText.getText().clear(); // Очистити EditText
+                hideKeyboard(); // Сховати клавіатуру
             } else {
-                Toast.makeText(getActivity(), "Table '" + tableName + "' already exists.", Toast.LENGTH_SHORT).show();
+                showCustomToastError();
             }
             db.close();
         } else {
-            Toast.makeText(getActivity(), "Failed to create table.", Toast.LENGTH_SHORT).show();
+            showCustomToastError2();
         }
     }
 
     private void showCustomToast() {
         LayoutInflater inflater = getLayoutInflater();
         View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) getView().findViewById(R.id.custom_toast_container));
-
         Toast toast = new Toast(getActivity().getApplicationContext());
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
+
+        // Отримати координати кнопки
+        int[] location = new int[2];
+        buttonBack.getLocationOnScreen(location);
+        int buttonX = location[0];
+        int buttonY = location[1];
+        // Встановити розташування Toast над кнопкою
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, buttonY - buttonBack.getHeight());
         toast.show();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> toast.cancel(), 900); // 1000 мс = 1 секунда
+    }
+
+    private void showCustomToastError() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_error, (ViewGroup) getView().findViewById(R.id.custom_toast_container));
+
+        // Налаштування повідомлення
+        TextView text = layout.findViewById(R.id.toast_message);
+        text.setText("einen solchen Namen gibt es");
+        // Використовуємо Toast для відображення спеціального повідомлення
+        Toast toast = new Toast(getActivity().getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        // Отримати координати кнопки
+        int[] location = new int[2];
+        buttonBack.getLocationOnScreen(location);
+        int buttonX = location[0];
+        int buttonY = location[1];
+        // Встановити розташування Toast над кнопкою
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, buttonY - buttonBack.getHeight());
+        toast.show();
+        // Використання Handler для приховування Toast через короткий проміжок часу
+        new Handler(Looper.getMainLooper()).postDelayed(toast::cancel, 1000); // 1000 мс = 1 секунда
+    }
+    private void showCustomToastError2() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_error, (ViewGroup) getView().findViewById(R.id.custom_toast_container));
+
+        // Налаштування повідомлення
+        TextView text = layout.findViewById(R.id.toast_message);
+        text.setText("Erstellen der Tabelle fehlgeschlagen.");
+        // Використовуємо Toast для відображення спеціального повідомлення
+        Toast toast = new Toast(getActivity().getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        // Отримати координати кнопки
+        int[] location = new int[2];
+        buttonBack.getLocationOnScreen(location);
+        int buttonX = location[0];
+        int buttonY = location[1];
+        // Встановити розташування Toast над кнопкою
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, buttonY - buttonBack.getHeight());
+        toast.show();
+        // Використання Handler для приховування Toast через короткий проміжок часу
+        new Handler(Looper.getMainLooper()).postDelayed(toast::cancel, 1000); // 1000 мс = 1 секунда
+    }
+    private void hideKeyboard() {
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
